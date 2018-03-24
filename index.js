7fda4e6c4f596af0431f896f75c14d9a35fd1acf
Ivan1906 <yanik.ivan@ukr.net>
2018-03-24 15:53:11 +0200

    Final project

--- index.js
+++ index.js
@@ -7,24 +7,52 @@ function convertCamelCaseToNameAttribute(nameAttribute) {
     }, "");
 }
 
+function isStringOrNumber(attribute) {
+    return typeof attribute === 'string' || typeof attribute === 'number';
+}
+
 function createElement(nameElement, attribute, innerElement) {
     let elementDOM = document.createElement(`${nameElement}`);
     if (elementDOM instanceof HTMLUnknownElement) 
-        throw Error(`Назва "${nameElement}" є не валідним тегом.`);
-
+        throw Error(`DOM об'єкт створений з імені "${nameElement.toUpperCase()}" є не валідним HTML тегом.`);
+        
     if (attribute !== undefined) {
         Object.entries(attribute).map(el => {
-
+            // Добавлення класу через "className"
             if (el[0].includes('className')) {
-                elementDOM.setAttribute('class', el[1]);
-            } else if (el[0].includes('textContent')){
-                elementDOM.textContent = el[1];
+                if (isStringOrNumber(el[0])) 
+                    elementDOM.className = `${el[1]}`; 
+                else throw Error(`Значення атрибуту "${el[0]}" для тегу "${nameElement.toUpperCase()}" повинен бути типом 'string' чи 'number'.`);
+            // Добавлення класу через "classList"       
+            } else if (el[0].includes('classList')) {
+                if (isStringOrNumber(el[0])) 
+                    elementDOM.classList = el[1]; 
+                else throw Error(`Значення атрибуту "${el[0]}" для тегу "${nameElement.toUpperCase()}" повинен бути типом 'string' чи 'number'.`);
+
+                if (Array.isArray(el[1])) 
+                    el[1].map(elem => {
+                        if (isStringOrNumber(el[0]))
+                            elementDOM.classList.add(`${elem}`); 
+                        else throw Error(`Значення масиву, атрибуту "${el[0]}" для тегу "${nameElement.toUpperCase()}" повині бути типом 'string' чи 'number'.`);
+                    });     
+            // Добавлення текстового вмісту елемента через "textContent"                    
+            } else if (el[0].includes('textContent')) {
+                typeof el[1] === 'object' && el[1] !== null ? 
+                    elementDOM.textContent = JSON.stringify(el[1]) : 
+                    elementDOM.textContent = el[1];
+            // Добавлення стилів елементу через "style"
             } else if (el[0].includes('style')) {
                 Object.entries(el[1]).map(elem => {
-                    elementDOM.style.setProperty(`${convertCamelCaseToNameAttribute(elem[0])}`, `${elem[1]}`);
+                    if (elementDOM.style[`${elem[0]}`] !== undefined) 
+                        if (isStringOrNumber(elem[1])) 
+                            elementDOM.style.setProperty(`${convertCamelCaseToNameAttribute(elem[0])}`, `${elem[1]}`); 
+                        else throw Error(`Значення стилю атрибуту "${elem[0]}" для тегу "${nameElement.toUpperCase()}" повинен бути типом 'string' чи 'number'.`);
+                    else throw Error(`Ім'я стилю "${elem[0]}" для тегу "${nameElement.toUpperCase()}" задано не вірно.`);
                 });
             } else {
-                elementDOM.setAttribute(`${convertCamelCaseToNameAttribute(el[0])}`, `${el[1]}`);
+                if (isStringOrNumber(el[1]))
+                    elementDOM.setAttribute(`${convertCamelCaseToNameAttribute(el[0])}`, `${el[1]}`);
+                else throw Error(`Значення атрибуту "${el[0]}" для тегу "${nameElement.toUpperCase()}" повинен бути типом 'string' чи 'number'.`);
             }
         });
     }
@@ -44,12 +72,15 @@ function createElement(nameElement, attribute, innerElement) {
 };
 
 function render(childElement, parentElement) {
-    if (!Array.isArray(parentElement) && 
-        parentElement instanceof HTMLElement && 
-        childElement instanceof HTMLElement) 
-    {
+    if (Array.isArray(parentElement) || 
+        parentElement instanceof HTMLUnknownElement || 
+        parentElement.nodeType !== Node.ELEMENT_NODE)
+        throw Error(`Батьківський елемент ${parentElement} не валідний.`);
+
+    if (childElement instanceof HTMLUnknownElement)
+        throw Error(`Батьківський елемент ${childElement.nodeName} не валідний.`);
+    
         parentElement.appendChild(childElement);
-    }
 }
 
 const React = {
